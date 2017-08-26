df4202b545138d6da018de6fbc8eed295530eeda
Raul Guaroa Montan Rodriguez <raulrodriguez1992@icloud.com>
2017-08-26 04:13:18 -0400

    Proyecto iniciado

---
+++ UseLockForms/UseLockForms/UseLock.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Windows.Forms;
+
+namespace UseLockForms
+{
+    public class UseLock
+    {
+        public static bool Access_ { get; set; }
+
+        public static void ManageAttempt()
+        {
+            RegistryKey rk1 = Registry.CurrentUser;
+            RegistryKey rk2 = Registry.CurrentUser;
+            string valor1;
+            rk1 = rk1.OpenSubKey("Microsoft_LA_RD", true);
+
+            if (rk1 == null)
+            {
+                RegistryKey rk3 = rk2.CreateSubKey("Microsoft_LA_RD");
+                rk3.SetValue("Attempt", "1", RegistryValueKind.String);
+                MessageBox.Show($"Este programa tiene un limite de 5 uso. cuando supere el limite debera digitar un numero de serie para continuar su uso", "Notificación de uso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Access_ = true;
+            }
+            else
+            {
+                valor1 = rk1.GetValue("Attempt").ToString();
+                switch (valor1)
+                {
+                    case "1":
+                        rk1.SetValue("Attempt", "2", RegistryValueKind.String);
+                        Access_ = true;
+                        break;
+                    case "2":
+                        rk1.SetValue("Attempt", "3", RegistryValueKind.String);
+                        Access_ = true;
+                        break;
+                    case "3":
+                        rk1.SetValue("Attempt", "4", RegistryValueKind.String);
+                        Access_ = true;
+                        break;
+                    case "4":
+                        rk1.SetValue("Attempt", "5", RegistryValueKind.String);
+                        Access_ = true;
+                        break;
+                    case "5":
+                        InputBox.SetLanguage(InputBox.Language.Spanish);
+                        DialogResult res = InputBox.ShowDialog($"Limite de uso superado, introdusca un numero de serie para continuar usando este programa: ",
+                            "Aplicacion Bloqueada",
+                            InputBox.Icon.Error,
+                            InputBox.Buttons.OkCancel,
+                            InputBox.Type.TextBox);
+                        if (res == DialogResult.OK)
+                        {
+                            while (InputBox.ResultValue.Length > 0 || String.IsNullOrEmpty(InputBox.ResultValue))
+                            {
+
+                                if (InputBox.ResultValue == "2509337")
+                                {
+
+                                    Access_ = true;
+                                    break;
+
+                                }
+                                else
+                                {
+                                    DialogResult res2 = InputBox.ShowDialog($"Numero de serie incorrecto!", "Aplicacion Bloqueada", InputBox.Icon.Error, InputBox.Buttons.OkCancel, InputBox.Type.TextBox, null, false, null);
+                                    if (res2 == DialogResult.Cancel)
+                                    {
+                                        Access_ = false;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        else if (res == DialogResult.Cancel)
+                        {
+                            Access_ = false;
+                            break;
+                        }
+
+                        //MessageBox.Show($"Ha superado la cantidad de uso que es de: {valor1} uso por usuario" , "Aplicacion Bloqueada", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+
+    }
+}
+
